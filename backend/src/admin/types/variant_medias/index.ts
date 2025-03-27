64ad7d5f8bece77e07e74180344d3970d6eedf05
Allen Yoo <ayoo123@umd.edu>
2025-03-27 01:29:32 -0400

    created types folder

---
+++ backend/src/admin/types/variant_medias/index.ts
@@ -0,0 +1,12 @@
+export type VariantMedia = {
+    id?: string;
+    file_id: string;
+    product_id: string;
+    variant_id: string;
+    url: string;
+    mime_type: string;
+    is_thumbnail: boolean;
+    name: string;
+    size: number;
+    file?: File;
+};
